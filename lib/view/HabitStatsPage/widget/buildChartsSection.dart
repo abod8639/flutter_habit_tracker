9c480cb45963e931ff36e69ed575f64773d31fe2
abot8639 <abot8639@gmail.com>
2025-06-13 04:21:10 +0300

    improve the code

--- lib/view/HabitStatsPage/widget/buildChartsSection.dart
+++ lib/view/HabitStatsPage/widget/buildChartsSection.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habit_tracker/controller/HabitController.dart';
-import 'package:habit_tracker/view/HabitStatsPage/data/HabitStats_data.dart';
 import 'package:habit_tracker/view/HabitStatsPage/widget/BuildBarChart.dart';
 import 'package:habit_tracker/view/HabitStatsPage/widget/BuildPieChart.dart';
 import 'package:habit_tracker/view/HabitStatsPage/widget/temp_file.dart';
 
 Widget buildChartsSection(BuildContext context, Map<String, dynamic> stats) {
-  // final List<Map<String, dynamic>> chartData = prepareChartData();
-  final trendSpots = prepareTrendData();
-  final trendLabels = prepareTrendLabels();
-  final maxTrendValue = getMaxTrendValue();
-
   return GetBuilder<HabitController>(
     builder: (controller) {
       return controller.isDesktop(context)

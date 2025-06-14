c7c8d588f790a9c1205f413bbd14d2c568d84a8e
abot8639 <abot8639@gmail.com>
2025-06-14 07:04:16 +0300

    delete showEditHabitDialog

--- lib/functions/HabitActions.dart
+++ lib/functions/HabitActions.dart
@@ -38,44 +38,6 @@ import 'package:habit_tracker/data/habit_db.dart';
 //   );
 // }
 
-// void showEditHabitDialog(
-//   int index,
-//   BuildContext context,
-//   TextEditingController controller,
-//   Habitdb db,
-//   VoidCallback update,
-// ) {
-//   if (index >= 0 && index < db.todaysHabitList.length) {
-//     controller.text = db.todaysHabitList[index][0];
-//     showDialog(
-//       context: context,
-//       builder: (context) {
-//         return Myalartd(
-//           hintText: 'Edit This Habit',
-//           controller: controller,
-//           onSave: () {
-//             final String habitName = controller.text.trim();
-//             if (habitName.isNotEmpty) {
-//               db.todaysHabitList[index][0] = habitName;
-//               db.updateData();
-//               Navigator.of(context).pop();
-//               update();
-//             } else {
-//               Get.snackbar(
-//                 'Error :',
-//                 'The field can’t be empty :)',
-//                 snackPosition: SnackPosition.BOTTOM,
-//                 backgroundColor: Colors.red.withOpacity(0.7),
-//                 colorText: Colors.white,
-//               );
-//             }
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
-
 void resetAllHabits(Habitdb db) {
   for (var habit in db.todaysHabitList) {
     habit[1] = false;

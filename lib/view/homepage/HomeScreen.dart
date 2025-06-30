bd99c56c4d68bd3f75ebc7191cea04a71cba6e22
abot8639 <abot8639@gmail.com>
2025-06-30 03:54:49 +0300

    add init to HomeScreen

--- lib/view/homepage/HomeScreen.dart
+++ lib/view/homepage/HomeScreen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<HabitController>(
+      init: HabitController(),
       builder: (controller) => _buildResponsiveLayout(context, controller),
     );
   }

9fe4de343e302402802a16f269cd7d191e1dc217
abot8639 <abot8639@gmail.com>
2025-08-12 22:44:51 +0300

    create SliverMonthlySummary

--- lib/view/homepage/Responsive/Phone.dart
+++ lib/view/homepage/Responsive/Phone.dart
@@ -55,27 +55,40 @@ class _PhoneState extends State<Phone> with SingleTickerProviderStateMixin {
                 slivers: [
                   // DrawerMenuButton(),
                   MyAppBar(),
-                  SliverToBoxAdapter(
-                    child: Center(
-                      child: SingleChildScrollView(
-                        reverse: true,
-                        scrollDirection: Axis.horizontal,
-                        physics: const BouncingScrollPhysics(),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: MonthlySummary(
-                            datasets: controller.db.heatmapDateSet,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  SliverMonthlySummary(),
                   HabitList(),
                 ],
               );
             },
           );
         }),
+      ),
+    );
+  }
+}
+
+class SliverMonthlySummary extends StatelessWidget {
+  const SliverMonthlySummary({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+      final HabitController controller = Get.put(HabitController());
+
+    return SliverToBoxAdapter(
+      child: Center(
+        child: SingleChildScrollView(
+          reverse: true,
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: MonthlySummary(
+              datasets: controller.db.heatmapDateSet,
+            ),
+          ),
+        ),
       ),
     );
   }
